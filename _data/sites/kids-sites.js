3a86c4a6975146c8bf6ba5fe78130ab5a014e65d
Russell Heimlich <kingkool68@gmail.com>
2020-12-31 23:38:27 -0500

    Add a site listing for my kids sites

---
+++ _data/sites/kids-sites.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: "Kids Sites", // optional, falls back to object key
+	description: "Each of my kids sites",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 12, // 12 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://zadieheimlich.com/",
+		"https://zadieheimlich.com/archives/",
+		"https://veraheimlich.com/",
+		"https://veraheimlich.com/archives/",
+		"https://cadenheimlich.com/",
+		"https://cadenheimlich.com/archives/",
+	]
+};
